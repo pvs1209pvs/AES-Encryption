9775b959db9d4b4bd33520710d8e511c084bd669
Paramvir Singh <ps17cw@brocku.ca>
2020-11-21 21:30:06 -0500

    user inputs made global constants

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <omp.h>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <sys/time.h>
 #include "AES.h"
 #include "utility.h"
 
@@ -9,7 +12,7 @@
  *      Paramvir Singh
  */
 
-const int NUM_BLOCKS = 200000;
+const int NUM_BLOCKS = 250000;
 const std::string AES_TYPE = "128";
 const int NUM_THREADS = 2;
 
@@ -18,9 +21,7 @@ int main() {
     // text file with random text
     fwrite_random(std::stoi(AES_TYPE), NUM_BLOCKS);
 
-
     // read plain text and key
-
     std::vector<std::vector<unsigned int>> input_text = fread_lines("../src/random_message.txt");
     std::vector<unsigned int> key = fread_chars("../src/key.txt");
    
@@ -34,14 +35,20 @@ int main() {
 
     // encryption
 
+    double start = omp_get_wtime();
+
     #pragma omp parallel for num_threads(NUM_THREADS)
     for (int i = 0; i < input_text.size(); i++){
         std::pair<unsigned int **, unsigned int **> key_msg = init(key, input_text.at(i), AES_TYPE);
         encrypted[i] = hex_mtrx_to_string(encrypt(key_msg.first, key_msg.second));
     }
 
+    double end = omp_get_wtime();
+    
+    std::cout << NUM_BLOCKS  << " blocks by " << NUM_THREADS << " threads done in " << (end-start)  << std::fixed << std::setprecision(5) << std::endl;
     std::cout << "\nEncrypted message saved in encrypted.txt" << std::endl;
 
     return 0;
 
 }
+
