2ef846b77dc3037ac0bdd4bf95e0528e9953856d
Fahad <fa16np@brocku.ca>
2020-11-29 20:33:17 -0500

    updates with a bug in MPI

--- MPI/src/AES.h
+++ MPI/src/AES.h
@@ -3,7 +3,6 @@
 #include <string>
 #include <vector>
 #include <map>
-#include "Configuration.h"
 
 extern int N;
 extern int ROUNDS;
--- MPI/src/main.cpp
+++ MPI/src/main.cpp
@@ -17,49 +17,83 @@
 
 
 int main(int argc, char* argv[]) {
-
+    
     const std::string AES_TYPE = "128";
-    const int NUM_BLOCKS = 4;
+    const int NUM_BLOCKS = 8;
 
     // Create a text file
-    fwrite_random(std::stoi(AES_TYPE), NUM_BLOCKS);
+    fwrite_random(std::stoi(AES_TYPE)/8, NUM_BLOCKS);
 
     // Read text and key
     std::vector<std::vector<unsigned int>> input_text = fread_lines("../src/random_message.txt");
+
+    std::cout << input_text.size() << " " << input_text.at(0).size() << std::endl;
+    unsigned int text [16*input_text.size()];
+
+    int text_sz = 0;
+    for (size_t i = 0; i < input_text.size(); i++) {
+        for (size_t j = 0; j < input_text.at(i).size(); j++) {
+            text[text_sz++] =  input_text.at(i).at(j);
+        }
+    }
+    
     std::vector<unsigned int> key = fread_chars("../src/key.txt");
+
+    std::pair<unsigned int **, unsigned int **> key_msg = init(key, input_text.at(0), "128");
+    std::cout<< hex_mtrx_to_string(encrypt(key_msg.first, key_msg.second)) << std::endl;
 
     char * encrypted[NUM_BLOCKS];
 
-    const int MAX_STRING = 96;
-
-    char block_buffer[MAX_STRING];
     int comm_sz;
     int my_rank;
-
-
+    
     MPI_Init(NULL, NULL);
 
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    if(my_rank != 0){
+    MPI_Datatype BLOCK;
 
-        std::pair<unsigned int *, unsigned int *> key_msg = init(key, input_text.at(my_rank-1), AES_TYPE);
-        std::string s = hex_mtrx_to_string(encrypt(key_msg.first, key_msg.second)).c_str();
+    int chunk_size = NUM_BLOCKS/4;
+    int block_array = 32*(chunk_size);
+    
+    MPI_Type_contiguous(block_array,MPI_CHAR,&BLOCK);
+    MPI_Type_commit(&BLOCK);
 
-        strcpy(block_buffer, s.c_str());
+    char buffer[block_array];
+    int buff_sz;
 
-        MPI_Send(block_buffer, strlen(block_buffer)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 
+    std::cout << "chunk " << chunk_size << std::endl;
+    std::cout << "block " << block_array << std::endl;
+
+
+
+    if (my_rank != 0){
+
+        buff_sz = 0;
+
+        for (int i = (my_rank-1)*chunk_size; i < my_rank*chunk_size ; ++i) {
+            buffer[buff_sz++] = text[i];
+        }
+
+        std::cout<< my_rank << std::endl;
+        for (int k = 0; k <buff_sz ; ++k) {
+            std::cout << buffer[k];
+        }
+//        for (int i=0; i<block_array; i++)
+//            buffer[i] = i;
+        MPI_Send(buffer, 1, BLOCK, 0, 0, MPI_COMM_WORLD);
     }
     else{
 
-        for (int i = 1; i < comm_sz; ++i){
-            MPI_Recv(block_buffer, MAX_STRING, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            encrypted[i-1] = block_buffer;
-            printf("%s \n", encrypted[i-1]);
+        for (int i = 1; i < comm_sz; i++) {
+            MPI_Recv(buffer, 1, BLOCK, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//            for (int j=0; j<block_array; j++)
+//                printf("%dbuffer[%d] = %d\n", i, j, buffer[j]);
         }
 
+        fflush(stdout);
     }
 
 
